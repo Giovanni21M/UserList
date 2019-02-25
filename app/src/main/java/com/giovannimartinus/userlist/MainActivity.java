128bb7296e5f7dcbd95826e1e3d7e99bb5de8426
Giovanni21M <giovanni21martinus@gmail.com>
2019-02-24 20:13:43 -0500

    Create DownLoadTask to scrape web data

--- app/src/main/java/com/giovannimartinus/userlist/MainActivity.java
+++ app/src/main/java/com/giovannimartinus/userlist/MainActivity.java
@@ -1,10 +1,18 @@
 package com.giovannimartinus.userlist;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +20,45 @@ public class MainActivity extends AppCompatActivity {
     private Button submitButton;
     private ListView listView;
     private RelativeLayout newUserLayout;
+
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection;
+
+            try {
+                // connect to browser with given url
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.connect();
+
+                // read&&download the input stream
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader streamReader = new InputStreamReader(inputStream);
+
+                int data = streamReader.read();
+
+                // add scraped web content to result
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = streamReader.read();
+                }
+
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
